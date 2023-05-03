3cdece1bcdc307e9ca88515e4a286e9e1c57f758
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-05-02 20:42:16 -0500

    added avatar to index users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tweets
-  resources :users, except: %i[index]
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ user1 = User.create(username: "user1", name: "user1", email: "user1@mail.com", p
 user2 = User.create(username: "user2", name: "user2", email: "user2@mail.com", password: "qwerty")
 user3 = User.create(username: "user3", name: "user3", email: "user3@mail.com", password: "qwerty")
 user4 = User.create(username: "user4", name: "user4", email: "user4@mail.com", password: "qwerty")
+user5 = User.create(username: "user5", name: "user5", email: "user5@mail.com", password: "qwerty")
 admin = User.create(username: "admin", name: "admin", email: "admin@mail.com", password: "qwerty", role: 1)
 puts "Creating Users Avatars"
 user1.avatar.attach(io: File.open("app/assets/images/icons_avatars/user1_icon.png"), filename:"user1_icon.png")
